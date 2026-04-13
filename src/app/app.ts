91828b8df66058da2af5efa523f294231f643744
Yury <shestsiaryn.yury@.gmail.com>
2026-04-13 20:43:47 +0300

    add buttom component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ButtonComponent } from './shared/button';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [ButtonComponent],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 })
--- src/app/shared/button.ts
+++ src/app/shared/button.ts
@@ -7,4 +7,4 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	standalone: true,
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Button {}
+export class ButtonComponent {}
